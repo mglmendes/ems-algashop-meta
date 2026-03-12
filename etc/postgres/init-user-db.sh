683ed845cd0d43ae1d4e1913411cdd4cd5a066f1
MGLMendes <miguelmendes203@gmail.com>
2026-03-12 15:20:17 -0300

    7.3. Conhecendo a API da Fastpay

--- etc/postgres/init-user-db.sh
+++ etc/postgres/init-user-db.sh
@@ -2,6 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE fastpay;
 	CREATE DATABASE ordering;
 	CREATE DATABASE billing;
+	CREATE DATABASE ordering_test;
+  CREATE DATABASE billing_test;
 EOSQL
